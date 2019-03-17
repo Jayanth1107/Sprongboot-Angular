85bac2d94f9f25376fd81a336a6fad52ed77e808
jayan <jayan@192.168.1.6>
2019-03-17 22:24:26 +0530

    Initial commit - no changes

---
+++ src/main/java/com/jayanth/angularJava/AngularJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jayanth.angularJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJavaApplication.class, args);
+	}
+
+}
